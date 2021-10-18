ac1cfcec35e5df9b61b37c949632e36d929947f3
Marica Marian-Constantin <marianmrc7@gmail.com>
2021-10-18 15:14:39 +0300

    Spring Initializr

---
+++ src/main/java/com/matrix/TeamMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamMatrixApplication.class, args);
+	}
+
+}
