7edb5a15b71b4ead1fdb9321e2fd7b05352917dd
Cristina Jipescu <cristina.cristina2109@gmail.com>
2021-10-30 11:08:21 +0300

    Create Administrator entity

---
+++ src/main/java/com/matrix/model/Administrator.java
@@ -0,0 +1,36 @@
+package com.matrix.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Administrator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    @Size(min = 8)
+    private String password;
+}
