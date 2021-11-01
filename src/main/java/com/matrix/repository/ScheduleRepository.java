52cea431d15dc111be85f499bd12bf1153273254
Marica Marian-Constantin <marianmrc7@gmail.com>
2021-11-01 09:46:19 +0200

    MVC Schedule: Repository, Service, Controller

---
+++ src/main/java/com/matrix/repository/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package com.matrix.repository;
+
+import com.matrix.model.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}
---
+++ src/main/java/com/matrix/service/ScheduleService.java
@@ -0,0 +1,15 @@
+package com.matrix.service;
+
+import com.matrix.repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    @Autowired
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
+}
