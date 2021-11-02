cb08abaf033ae3f575fb58eb5b71da74d78f4250
Cristina Jipescu <cristina.cristina2109@gmail.com>
2021-11-02 08:35:36 +0200

    Add diagrams

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,16 @@
+function showDiv() {
+    document.getElementById('registerModal').style.display='block';
+ }
+
+ function closeDiv() {
+    document.getElementById('registerModal').style.display='none';
+ }
+
+ function closeDivLogin() {
+    document.getElementById('loginModal').style.display='none';
+ }
+
+function login(){
+    document.getElementById('registerModal').style.display='none';
+    document.getElementById('loginModal').style.display='block';
+}
