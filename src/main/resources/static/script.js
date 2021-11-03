5cf6cec210e93ab524c431ff9b7417b68be384a9
Cristina Jipescu <cristina.cristina2109@gmail.com>
2021-11-03 09:46:53 +0200

    Resolve bug of displaying modals. Add diagram of classes.

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,7 +1,12 @@
 function showDiv() {
     document.getElementById('registerModal').style.display='block';
+    document.getElementById('loginModal').style.display='none';
  }
 
+function login(){
+    document.getElementById('registerModal').style.display='none';
+    document.getElementById('loginModal').style.display='block';
+}
  function closeDiv() {
     document.getElementById('registerModal').style.display='none';
  }
@@ -10,7 +15,3 @@ function showDiv() {
     document.getElementById('loginModal').style.display='none';
  }
 
-function login(){
-    document.getElementById('registerModal').style.display='none';
-    document.getElementById('loginModal').style.display='block';
-}
